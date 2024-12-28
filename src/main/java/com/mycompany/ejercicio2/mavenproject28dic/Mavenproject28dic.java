6ede2e79369bbd68e011de2baa114a12524b62d3
Bkad0190 <paosafin@gmail.com>
2024-12-28 11:58:47 -0500

    Update Mavenproject28dic.java

--- src/main/java/com/mycompany/ejercicio2/mavenproject28dic/Mavenproject28dic.java
+++ src/main/java/com/mycompany/ejercicio2/mavenproject28dic/Mavenproject28dic.java
@@ -9,8 +9,33 @@ package com.mycompany.ejercicio2.mavenproject28dic;
  * @author ItsqmetEstudiantes
  */
 public class Mavenproject28dic {
+ public static void main(String[] args) {
+        String nombre = "";
+        int edad = 0;
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+        double notas[] = {7, 10, 8, 9, 6, 2, 4};
+        double notas2[] = {7, 10, 6, 9};
+        nombre = JOptionPane.showInputDialog("Ingresar nombre");
+        edad = Integer.parseInt(JOptionPane.showInputDialog("Ingresar edad"));
+
+        mensaje(nombre, edad);
+        JOptionPane.showMessageDialog(null, "el promedio es" + promedio(notas));
+        JOptionPane.showMessageDialog(null, "el promedio es" + promedio(notas2));
     }
+
+    public static void mensaje(String nombres, int edades) {
+        JOptionPane.showMessageDialog(null, nombres + " tiene " + edades + "años ");
+    }
+
+    static double promedio(double[] calificaciones) {
+
+        double suma = 0;
+        double respuesta = 0;
+        for (int i = 0; i < calificaciones.length; i++) {
+            suma = suma + calificaciones[i];
+        }
+        respuesta = suma / calificaciones.length;
+        return respuesta;
+    }
+
 }
