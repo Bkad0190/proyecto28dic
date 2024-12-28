46c3d4e10b21ace68c7a216fd331b45b67025949
kadrian96 <w.kevinm@hotmail.com>
2024-12-28 11:39:13 -0500

    primer commit

---
+++ src/main/java/com/mycompany/ejercicio2/mavenproject28dic/Mavenproject28dic.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio2.mavenproject28dic;
+
+/**
+ *
+ * @author ItsqmetEstudiantes
+ */
+public class Mavenproject28dic {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
